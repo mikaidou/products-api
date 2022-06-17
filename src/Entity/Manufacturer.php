57f5b1140b88b988ce71924a79b1180b2621f2b9
mikaidou <mikaidoumbo92@gmail.com>
2022-06-17 11:32:07 +0200

    add manufacturer ressource

---
+++ src/Entity/Manufacturer.php
@@ -0,0 +1,109 @@
+<?php
+
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/** A manufacturer */
+#[ApiResource]
+class Manufacturer
+{
+    /** The id of the manufacturer */
+    private ?int $id = null;
+
+    /** The name of the manufacturer */
+    private string $name = '';
+
+    /** The description of the manufacturer */
+    private string $description = '';
+
+    /** The country code of the manufacturer */
+    private string $countryCode = '';
+
+    /** The date that the manufacturer was listed */
+    private ?\DateTimeInterface $listedDate = null;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryCode(): string
+    {
+        return $this->countryCode;
+    }
+
+    /**
+     * @param string $countryCode
+     */
+    public function setCountryCode(string $countryCode): void
+    {
+        $this->countryCode = $countryCode;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getListedDate(): ?\DateTimeInterface
+    {
+        return $this->listedDate;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $listedDate
+     */
+    public function setListedDate(?\DateTimeInterface $listedDate): void
+    {
+        $this->listedDate = $listedDate;
+    }
+
+
+
+}
